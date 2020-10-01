0cb0443bf546a8d32bee8cb8f869ecfdfd22eea1
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-10-01 18:06:28 -0400

    added comment section of site

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,13 +10,33 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createAtVal) => moment(createAtVal).format('MMM DD, YYY [at] hh:mm a')
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+// get total count of comments and replies on retrievals
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 // create the Pizza model usign the PizzaSchema
